9b6b936a260cb1531c36207db9482f3168eabb4a
amir830047agh@gmail.com <amir830047agh@gmail.com>
2025-12-14 01:02:14 +0330

    Update Footer, Contact, and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from './sections/About'
 import Contact from './sections/Contact'
 import Experience from './sections/Experience'
+import Footer from './sections/Footer'
 import Hero from './sections/Hero'
 import Navbar from './sections/Navbar'
 import Projects from './sections/Projects'
@@ -17,6 +18,7 @@ function App() {
         <Experience />
         <Testimonial />
         <Contact />
+        <Footer />
       </div>
     </>
   )
